487a5bd1a228d5347025adab3dadbff7b5a2b135
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-06-03 14:53:57 +0200

    test deploy

--- app.js
+++ app.js
@@ -2,20 +2,6 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://funny-croquembouche-d10144.netlify.app"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header(
-    "Access-Control-Expose-Headers",
-    "https://funny-croquembouche-d10144.netlify.app"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 require("dotenv").config(); // To load environment variables from .env
 
 //Cors
@@ -29,6 +15,15 @@ app.use(
     credentials: true,
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 const cookieParser = require("cookie-parser");
 
