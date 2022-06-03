009368ae9354338a5a78f38eec897cfd552973bb
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-06-03 15:43:37 +0200

    notning to commit

--- app.js
+++ app.js
@@ -13,19 +13,6 @@ app.use(
   })
 );
 
-/* app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://funny-croquembouche-d10144.netlify.app"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  res.header(
-    "Access-Control-Expose-Headers",
-    "https://funny-croquembouche-d10144.netlify.app"
-  );
-  next();
-}); */
-
 const cookieParser = require("cookie-parser");
 
 //Database
