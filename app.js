5a15a11fe8ac8a2e325f1fa0cb2d32d3c494aa43
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-06-03 14:22:18 +0200

    test deploy

--- app.js
+++ app.js
@@ -2,6 +2,13 @@
 const express = require("express");
 const app = express();
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Expose-Headers", "*");
+  next();
+});
+
 require("dotenv").config(); // To load environment variables from .env
 
 //Cors
